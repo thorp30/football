aa9453fb079abd4b914e69d28875f3586e023065
tomthorp30 <86660385+tomthorp30@users.noreply.github.com>
2023-01-25 09:16:49 +0000

    Update predicting_football_data_v1.py

--- predicting_football_data_v1.py
+++ predicting_football_data_v1.py
@@ -8,27 +8,34 @@ Created on Fri Jan  6 16:00:49 2023
 
 """
 
-This script takes a csv file of information relating to football matches, formats the data into a more usable format, then uses
-different ML appraoches to try to predict whether the teams will win their next game. 
-
+This script takes a csv file of information relating to football matches (output from scraping_football_data_v2.py), 
+formats the data into a more usable format, introduces feature selection, then uses different ML appraoches to try to 
+predict whether the teams will win their next game. 
 
 """
 
 # Importing libraries
 import pandas as pd
 import numpy as np
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
 
-rf = RandomForestClassifier(n_estimators=50, min_samples_split=10, random_state=1)
 
 #read in previously made csv file
 matches = pd.read_csv(r"C:\Users\tt13\football\matches_20230106.csv", index_col=0)
 
 """
 
-This next section drops the variables which will not be used and creates predictor variables which will be of use. 
+This next section first drops the variables which will not be used and  then creates predictor variables which will be of use going forward. 
+This is done by creating a function to iterate through defined functions and create a rolling average version of the variable. 
+This is currently set to a 3 rolling average.  
+
+This section is adapted from: 
+https://github.com/dataquestio/project-walkthroughs/blob/master/football_matches/prediction.ipynb
 
 """
 
@@ -38,10 +45,10 @@ matches = matches.drop(columns=['Captain','Formation','Referee','Match Report', 
 #convert date column into datetime 
 matches["Date"] = pd.to_datetime(matches["Date"])
 
-#convert the string venue variable into integer categories
+#convert the string venue variable into integer categories and create a unique indicator for each outcome option. 
 matches["venue_code"] = matches["Venue"].astype("category").cat.codes
 
-#convert the string opponent variable into integer categories
+#convert the string opponent variable into integer categories and create a unique indicator for each outcome option.
 matches["opp_code"] = matches["Opponent"].astype("category").cat.codes
 
 #Create a target column, which is when the result column shows W (0,1 for L, W) 
@@ -50,12 +57,14 @@ matches["target"] = (matches["Result"] == "W").astype("int")
 #convert the string result variable into integer categories (0,1,2 for L,D,W)
 matches["result_code"] = matches["Result"].astype("category").cat.codes
 
+
 #The next section creates 3-match rolling variables to account for form
 
 #create a group of df for each team 
 grouped_matches = matches.groupby("Team")
 
-#Create a function which takes each team in grouped_matches and creates additional 3 match rolling averages for each team. 
+#Create a function which takes each team in grouped_matches and creates additional 3 match rolling averages for each team for all
+#variables defined in cols. 
 
 def rolling_averages(group, cols, new_cols):
     group = group.sort_values("Date")
@@ -81,13 +90,60 @@ matches_rolling.index = range(matches_rolling.shape[0])
 
 """
 
+This next section undertakes feature selection. This enables automatic selection of the features within the data that contribute most 
+to the predictor variable to be selected. Feature selection can lead to a reduction in overfitting, imrpove model accuracy and reduce training
+time if there is a large dataset.
+
+First feature selection undertaken is univariate selection 
+Second feature selection undertaken is principal component analysis
+
+(Feature selection adapted from Jason Brownlee book "Machine Learning Mastery with Python") 
+
+"""
+
+#Undertaking univariate selection
+
+#Convert dataframe into np array
+array = matches_rolling.values
+
+#Provide the location within the array of all potential predictor variables, and the location of target variable
+predictor_loc = [42,43,46,47,48,49,50,51,52,53,54,55,56]
+target_loc = [44]
+
+#Seperate array for predictor variables and target variable
+predictor_variables = array[:,predictor_loc]
+target_variable = array[:,target_loc].astype('int') #target must be an integer 
+
+# feature extraction
+test = SelectKBest(score_func=chi2, k=4)
+fit = test.fit(predictor_variables, target_variable)
+
+# summarize scores
+np.set_printoptions(precision=3)
+print(fit.scores_)
+features = fit.transform(predictor_variables)
+# summarize selected features
+print(features[0:5,:])
+
+
+
+
+
+
+
+
+"""
+
 This next section trains the ML model to create a set of predictions using the previously created predictor variables. 
 
 """
 
+
+rf = RandomForestClassifier(n_estimators=50, min_samples_split=10, random_state=1)
+
+
 #Create a list of predictor variables
 predictors = ["venue_code", "opp_code"] + new_cols
-
 
 #Create predicting function 
 def make_predictions(data, predictors):
